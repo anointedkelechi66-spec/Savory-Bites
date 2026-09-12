5453e56c2416feaf00d79c0d7ef9c4e24703c3eb
Anointed <anointedkelechi66@gmail.com>
2026-09-12 14:23:37 +0100

    created  the initial webpage with the readme

---
+++ script.js
@@ -0,0 +1,9 @@
+// Savory Bites - Initial JavaScript
+
+console.log("Welcome to Savory Bites!");
+
+
+// Display a message when the page has loaded
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Savory Bites webpage loaded successfully.");
+});
